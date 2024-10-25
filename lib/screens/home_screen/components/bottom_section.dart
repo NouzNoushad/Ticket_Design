310fd96fe7463e81887f5dbb175296345a00ef7a
unknown <nhd7noushadazeez@gmail.com>
2024-10-25 22:04:40 +0530

    bottom section

--- lib/screens/home_screen/components/bottom_section.dart
+++ lib/screens/home_screen/components/bottom_section.dart
@@ -47,14 +47,105 @@ class _TripBottomSectionState extends State<TripBottomSection>
         ),
         Expanded(
             child: TabBarView(controller: _tabController, children: [
-          Container(
-            color: Colors.blue,
-          ),
+          _buildUpcomingContainer(),
           Container(
             color: Colors.yellow,
           ),
         ]))
       ],
     );
+  }
+
+  Widget _buildUpcomingContainer() {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double padding = 8.0;
+    return PageView.builder(
+        controller:
+            PageController(viewportFraction: 0.95 + (padding / screenWidth)),
+        itemCount: 3,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.only(left: padding, right: padding),
+            child: LayoutBuilder(builder: (context, constraints) {
+              return Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    children: [
+                      Expanded(
+                          flex: 3,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          )),
+                      const Expanded(
+                          child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 8,
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                                child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Travel Date',
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      color: Color.fromRGBO(117, 97, 83, 1)),
+                                ),
+                                Text(
+                                  '11 Oct 24',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      color: Color.fromRGBO(117, 97, 83, 1)),
+                                ),
+                              ],
+                            )),
+                            Expanded(
+                                child: Center(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Departure',
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        color: Color.fromRGBO(117, 97, 83, 1)),
+                                  ),
+                                  Text(
+                                    '1:30PM',
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        color: Color.fromRGBO(117, 97, 83, 1)),
+                                  ),
+                                ],
+                              ),
+                            )),
+                            Expanded(
+                              child: Align(
+                                alignment: Alignment.centerRight,
+                                child: Text(
+                                  'View Details',
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      color: Color.fromRGBO(117, 97, 83, 1)),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )),
+                    ],
+                  ));
+            }),
+          );
+        });
   }
 }
